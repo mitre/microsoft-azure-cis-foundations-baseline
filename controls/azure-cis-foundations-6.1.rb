298a19fd550cfdc9a336d451f027817c58f88f5a
Aaron Lippold <lippold@gmail.com>
2020-02-13 12:21:36 -0500

    - pushed the test for control 6.1
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/azure-cis-foundations-6.1.rb
+++ controls/azure-cis-foundations-6.1.rb
@@ -1,12 +1,14 @@
-control "azure-cis-9.2-control-6.1" do
-  title "Ensure that RDP access is restricted from the internet"
-  desc  "Disable RDP access on network security groups from the Internet."
-  desc  "rationale", "The potential security problem with using RDP over the
+# frozen_string_literal: true
+
+control 'azure-cis-9.2-control-6.1' do
+  title 'Ensure that RDP access is restricted from the internet'
+  desc  'Disable RDP access on network security groups from the Internet.'
+  desc  'rationale', "The potential security problem with using RDP over the
 Internet is that attackers can use various brute force techniques to gain
 access to Azure Virtual Machines. Once the attackers gain access, they can use
 a virtual machine as a launch point for compromising other machines on an Azure
 Virtual Network or even attack networked devices outside of Azure."
-  desc  "check", "
+  desc  'check', "
     **Azure Console**
 
     1. For each VM, open the `Networking` blade
@@ -36,7 +38,7 @@ such as
 \"internet\" or \"any\"
     ```
   "
-  desc  "fix", "
+  desc 'fix', "
     Disable direct RDP access to your Azure Virtual Machines from the Internet.
 After direct RDP access from the Internet is disabled, you have other options
 you can use to access these virtual machines for remote management:
@@ -50,17 +52,17 @@ VPN](https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-howto-site-t
      - [ExpressRoute](https://docs.microsoft.com/en-us/azure/expressroute/)
   "
   impact 0.5
-  tag severity: "medium"
+  tag severity: 'medium'
   tag gtitle: nil
   tag gid: nil
   tag rid: nil
   tag stig_id: nil
   tag fix_id: nil
   tag cci: nil
-  tag nist: ["CM-7 (1)", "Rev_4"]
+  tag nist: ['CM-7 (1)', 'Rev_4']
   tag cis_level: 1
-  tag cis_controls: ["9.2", "Rev_7"]
-  tag cis_rid: "6.1"
+  tag cis_controls: ['9.2', 'Rev_7']
+  tag cis_rid: '6.1'
   tag false_negatives: nil
   tag false_positives: nil
   tag documentable: nil
@@ -71,5 +73,12 @@ VPN](https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-howto-site-t
   tag mitigation_controls: nil
   tag responsibility: nil
   tag ia_controls: nil
-end
 
+  azurerm_resource_groups.names.each do |rg_name|
+    azurerm_network_security_group(resource_group: rg_name).names.each do |nsg_name|
+      describe azurerm_network_security_group(resource_group: rg_name, name: nsg_name) do
+        it { should_not allow_rdp_from_internet }
+      end
+    end
+  end
+end
