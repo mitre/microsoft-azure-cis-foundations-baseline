bb77008b24a6ea0df2e48bbbad4ee239653b0189
Ben Cresitello-Dittmar <bcresitellodittmar@mitre.org>
2020-03-03 12:07:52 -0500

    Typo, 'group' should have been 'groups'

--- controls/azure-cis-foundations-6.2.rb
+++ controls/azure-cis-foundations-6.2.rb
@@ -75,7 +75,7 @@ VPN](https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-howto-site-t
   tag ia_controls: nil
 
   azurerm_resource_groups.names.each do |rg_name|
-    azurerm_network_security_group(resource_group: rg_name).names.each do |nsg_name|
+    azurerm_network_security_groups(resource_group: rg_name).names.each do |nsg_name|
       describe azurerm_network_security_group(resource_group: rg_name, name: nsg_name) do
         it { should_not allow_ssh_from_internet }
       end
