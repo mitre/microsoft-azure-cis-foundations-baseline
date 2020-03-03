fcef1bc2d636ff0a633d703d73ab49404479481c
Lipee Vora <lvora@mitre.org>
2020-03-03 12:41:56 -0500

    3.2 is complete, 3.5 and 3.8 are still in progress (need debugging + fixing)

--- controls/azure-cis-foundations-3.2.rb
+++ controls/azure-cis-foundations-3.2.rb
@@ -70,6 +70,13 @@ account access keys."
   tag responsibility: nil
   tag ia_controls: nil
 
+  azurerm_resource_groups.names.each do |rg_name|
+    azurerm_storage_accounts(resource_group: rg_name).names.each do |sa_name|
+      describe azurerm_storage_account(resource_group: rg_name, name: sa_name) do
+        its('has_recently_generated_access_key?') {should be true}
+      end
+    end
+  end
 
 end
 
